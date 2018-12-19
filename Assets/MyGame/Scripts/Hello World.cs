77875bf041ff652d2c7b48f53bf3fd8fcf0f79ea
Kuhn <fabian.kuhn@htl-salzburg.ac.at>
2018-12-19 10:25:38 +0100

    init project

---
+++ Assets/MyGame/Scripts/Hello World.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World");
+		
+		//Aufruf Methode
+		float number = getRandom();
+		Debug.Log("random number: " + number);
+	}
+	
+	//deklaration Methode
+	float getRandom (){
+		return UnityEngine.Random.Range(0f,10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
